4f72749005f7d3410e024994d258879e3ab2e6b3
Dmitri Naumov <ru.vinzent@gmail.com>
2012-04-19 13:21:35 +0600

    More widgets

--- src/sweety/core.clj
+++ src/sweety/core.clj
@@ -176,17 +176,40 @@
      'setLocation vec->pt
      'setBounds vec->rect}))
 
-(defwidget org.eclipse.swt.widgets.Composite)
-(defwidget org.eclipse.swt.widgets.Label)
+(defwidget org.eclipse.swt.browser.Browser)
+
 (defwidget org.eclipse.swt.widgets.Button)
-(defwidget org.eclipse.swt.widgets.List)
 (defwidget org.eclipse.swt.widgets.Canvas)
-(defwidget org.eclipse.swt.widgets.FileDialog)
+(defwidget org.eclipse.swt.widgets.Combo)
+(defwidget org.eclipse.swt.widgets.Composite)
+(defwidget org.eclipse.swt.widgets.CoolBar)
+(defwidget org.eclipse.swt.widgets.DateTime)
+(defwidget org.eclipse.swt.widgets.ExpandBar)
 (defwidget org.eclipse.swt.widgets.Group)
-(defwidget org.eclipse.swt.widgets.Scale)
-(defwidget org.eclipse.swt.widgets.Text)
-(defwidget org.eclipse.swt.widgets.MessageBox)
+(defwidget org.eclipse.swt.widgets.Label)
+(defwidget org.eclipse.swt.widgets.Link)
+(defwidget org.eclipse.swt.widgets.List)
+(defwidget org.eclipse.swt.widgets.Menu)
+(defwidget org.eclipse.swt.widgets.ProgressBar)
+(defwidget org.eclipse.swt.widgets.Sash)
 (defwidget org.eclipse.swt.widgets.Shell)
+(defwidget org.eclipse.swt.widgets.Slider)
+(defwidget org.eclipse.swt.widgets.Scale)
+(defwidget org.eclipse.swt.widgets.Spinner)
+(defwidget org.eclipse.swt.widgets.TabFolder)
+(defwidget org.eclipse.swt.widgets.Table)
+(defwidget org.eclipse.swt.widgets.Text)
+(defwidget org.eclipse.swt.widgets.ToolBar)
+(defwidget org.eclipse.swt.widgets.Tray)
+(defwidget org.eclipse.swt.widgets.Tree)
+
+(defwidget org.eclipse.swt.widgets.FileDialog)
+(defwidget org.eclipse.swt.widgets.MessageBox)
+
+;;;  Custom
+(defwidget org.eclipse.swt.custom.CTabFolder)
+(defwidget org.eclipse.swt.custom.ScrolledComposite)
+(defwidget org.eclipse.swt.custom.StyledText)
 
 
 ;;; Utilities ------------------------------------------------------------------
